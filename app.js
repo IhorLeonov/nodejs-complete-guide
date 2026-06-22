c01831bf271c80649861a9f95d26bef90bfe5c14
IhorLeonov <ihorleonow@gmail.com>
2026-06-22 13:15:08 +0300

    Add assignment 2

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("middleware 1");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("middleware 2");
+  res.send("<h1>Add product</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("middleware 2");
   res.send("<h1>Hello from Express!</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3001);
+app.listen(3001);
