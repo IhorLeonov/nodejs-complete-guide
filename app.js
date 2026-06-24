403629ecc46bb9727eaf4d9110dab6a156cb5e9c
IhorLeonov <ihorleonow@gmail.com>
2026-06-24 13:38:21 +0300

    Add routes

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  console.log("middleware 2");
-  res.send("<h1>Add product</h1>");
-});
+const adminRoutes = require("./routes/admin");
+const showRoutes = require("./routes/shop");
 
-app.use("/", (req, res, next) => {
-  console.log("middleware 2");
-  res.send("<h1>Hello from Express!</h1>");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(showRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3001);
