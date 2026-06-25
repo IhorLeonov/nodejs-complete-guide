53f24250ccaf85cfa21cf5ac7151412b1ef985f1
IhorLeonov <ihorleonow@gmail.com>
2026-06-25 14:15:53 +0300

    Add html

--- other/assignments/2/app.js
+++ other/assignments/2/app.js
@@ -3,16 +3,13 @@ const path = require("path");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
-const showRoutes = require("./routes/shop");
+const usersRoutes = require("./routes/users");
+const homeRoutes = require("./routes/home");
 
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
-app.use(showRoutes);
+app.use(usersRoutes);
+app.use(homeRoutes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
