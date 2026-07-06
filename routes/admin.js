1c815fa85991e97b0ce4194833c21bbbf73c9701
IhorLeonov <ihorleonow@gmail.com>
2026-07-06 13:13:21 +0300

    Add pug tamplates

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,20 @@
-const express = require("express");
 const path = require("path");
-const rootDir = require("../utils/path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
-// GET
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product", { pageTitle: "Add product" });
 });
 
-// POST
+// /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect("/");
